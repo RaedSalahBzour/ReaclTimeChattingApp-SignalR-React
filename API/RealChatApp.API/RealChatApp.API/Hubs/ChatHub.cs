efe26e37967989518cac0709512321b534303dba
Raed Salah Bzour <raedbzour77@gmail.com>
2025-09-24 11:50:33 +0300

    adding api and creating client

---
+++ API/RealChatApp.API/RealChatApp.API/Hubs/ChatHub.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using RealChatApp.API.Models;
+
+namespace RealChatApp.API.Hubs;
+
+public class ChatHub : Hub
+{
+    public async Task JoinChat(UserConnection connection)
+    {
+        await Clients.All
+            .SendAsync("ReceiveMessage", "admin", $"{connection.UserName} has joined");
+    }
+    public async Task JoinSpecificGroup(UserConnection connection)
+    {
+        await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
+        await Clients.Group(connection.ChatRoom)
+            .SendAsync("ReceiveMessage", "admin",
+            $"{connection.UserName} has joined {connection.ChatRoom}");
+    }
+}
